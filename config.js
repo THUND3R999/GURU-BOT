eb1cd5f2d7a5ea95f5375bf0ec500334bc25f9d5
THUND3R999 <153846992+THUND3R999@users.noreply.github.com>
2023-12-15 01:52:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['0714734939', 'THUND3R BOT', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
